deb85f301a3846603e520a396c59d0d4b2312ae6
ozodbek1001 <sherbekovozodbek747@gmail.com>
2024-12-16 21:11:02 +0500

    bajarildi

---
+++ user.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self,phone,is_admin,is_client,password):
+        self.phone=phone
+        self.is_admin=is_admin
+        self.is_client=is_client
+        self.passwoerd=password
+        self.ad=[]
