f2db60c4664c85a9f06f664b600982615843c86b
ozodbek1001 <sherbekovozodbek747@gmail.com>
2024-12-18 19:16:06 +0500

    qoshildi

---
+++ dokon.py
@@ -0,0 +1,9 @@
+class Dokon:
+    def __init__(self,nomi,location):
+        self.nomi=nomi
+        self.location=location
+        self.balanc=0
+        self.maxsulot_baza=[]
+
+    def info_dokon(self):
+        return f"nomi : {self.nomi}\n"
